5e25587faabfef57662b00557ac69184f874c002
soundstate-app <companionaisoftwaredev@gmail.com>
2026-01-26 17:49:36 -0500

    bkup

---
+++ src/pages/BandDjDemo.tsx
@@ -0,0 +1,15 @@
+export default function BandDjDemo() {
+  return (
+    <iframe
+      title="Band / DJ Template Demo"
+      src="/templates/band-dj/index.html"
+      style={{
+  width: '100%',
+  height: '100vh',
+  border: 0,
+  display: 'block'
+}}
+
+    />
+  );
+}
